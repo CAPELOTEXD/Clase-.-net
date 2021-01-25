0d2181c27e90ef0ff31e62bbc77d36773acb0ba4
CAPELOTEXD <48354495+CAPELOTEXD@users.noreply.github.com>
2021-01-25 16:41:39 -0500

    Add files via upload

---
+++ toma y muestra de datos/toma y muestra de datos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace toma_y_muestra_de_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            int edad;
+
+            Console.WriteLine("digite su nombre");
+
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("digite su apellido");
+
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("digite su edad");
+
+            edad = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("señor {0} {1} con edad de {2} años. bienvenido ", nombre, apellido, edad);
+
+            Console.ReadLine();
+        }
+    }
+}
