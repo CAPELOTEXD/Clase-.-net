90598e69d1f0f09f878696262b02cf3e67f6b2a4
CAPELOTEXD <48354495+CAPELOTEXD@users.noreply.github.com>
2021-01-25 16:33:36 -0500

    Add files via upload

---
+++ clases de herencia/clases de herencia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clases_de_herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
