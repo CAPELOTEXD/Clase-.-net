44d7b5bcadfd7f08715491c297722597f4382ce4
CAPELOTEXD <48354495+CAPELOTEXD@users.noreply.github.com>
2021-01-25 16:40:17 -0500

    Add files via upload

---
+++ nombre_reves/datoss.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class datos
+{
+	public datos()
+	{
+	}
+}
---
+++ nombre_reves/nombre_reves/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+namespace nombre_reves
+{
+    class Program
+    {
+       
+        private String user;
+        private String vernombre = "";
+        private int n;
+        private String[] reves;        
+        private String[] reverso;
+        private string[] total ;
+        
+
+        public Program(string user)
+        {
+            this.user = user;
+            n = user.Length;
+            reves = new string[n];
+            reverso = new string[n];
+        }
+
+        private String voltear()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (Char.IsLetter(user[i]))
+                {
+                    reves[i] = Convert.ToString(user[i]);
+                }
+            }
+
+          
+
+            for (int i = reves.Length-1; i <= reves.Length; i--)
+            {
+                if (i == -1)
+                {
+                    break;
+                }
+
+                reverso[i] = reves[i];
+                vernombre = $"{vernombre}{ reverso[i]}";
+               
+            }
+
+            return vernombre;
+        
+        }
+
+
+
+        static void Main()
+        {
+            Console.WriteLine("Digita tu Nombre..");
+            
+            var user = Console.ReadLine();
+            var casos = new Program(user);
+
+            Console.WriteLine("Su nombre al reves es: " + casos.voltear());
+        }
+
+       
+
+        
+
+    }
+}
