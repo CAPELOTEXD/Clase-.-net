d089f0c377ced06a8b69aae3b7f9600d370c23e7
CAPELOTEXD <48354495+CAPELOTEXD@users.noreply.github.com>
2021-01-25 16:35:28 -0500

    Add files via upload

---
+++ envio datos a travez de array y metodos/envio datos a travez de array y metodos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace envio_datos_a_travez_de_array_y_metodos
+{
+    
+    class Program
+    {           
+
+      
+        static void Main()
+        {
+            object[] parametros = {"Tavo",27,1023929376};
+            new Program().metodo(parametros);
+            Console.ReadLine();
+            
+            
+        }
+
+        private void metodo(object[] parametros)
+        {
+            var id = parametros[2];
+            String nombre = Convert.ToString(parametros[0]);
+            var edad = parametros[1];
+
+        }
+    }
+}
